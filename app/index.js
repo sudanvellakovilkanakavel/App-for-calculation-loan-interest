16c422e700373a021dec4bf61f85c2fc8b08c472
Sudan Vellakovil Kanaka Vel <sudan.kanakavel@maplebots.com>
2016-04-06 16:02:25 +0530

    Added files via upload

--- app/index.js
+++ app/index.js
@@ -7,10 +7,46 @@ var HelloWorld = React.createClass({
     render: function () {
 
     	return (
-            <div> An Effective  App For Calculating Your Loan Interest</div>
+            <div> 
+             <form>
+            <h2>An Effective  App For Calculating Your Loan Interest </h2>
+            <br />
+            <br />
+            <br />
+           
+            Enter loan amount : &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="text"  />
+            <br />
+            <br />
+            Enter  interest rate: &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="text"  />
+             <br />
+             <br />
+            Enter duration in months: &nbsp;
+            <input type="text"  />
 
-            SAelect bank 
+            <br />
+              <br />
+              <br />
 
+            <button
+className="btn btn-block btn-success"
+type="submit">
+Calculate The Aount Payable on Each Month
+</button>
+<br />
+              <br />
+              <br />
+
+              Amount payable on each month is (Interest+Principal):
+            </form>
+
+
+
+
+            </div>
+
+           
     		)
     }
 
