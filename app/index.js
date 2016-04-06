ccd51de2f9d1b27b6a6887f2a93addc4ef344393
Sudan Vellakovil Kanaka Vel <sudan.kanakavel@maplebots.com>
2016-04-06 14:57:46 +0530

    Added files via upload

---
+++ app/index.js
@@ -0,0 +1,29 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var HelloWorld = React.createClass({
+
+    render: function () {
+
+    	return (
+            <div> An Effective  App For Calculating Your Loan Interest</div>
+
+            SAelect bank 
+
+    		)
+    }
+
+
+});
+
+
+ReactDOM.render(
+
+<HelloWorld  />,
+document.getElementById('app')
+
+
+	);
+
+
