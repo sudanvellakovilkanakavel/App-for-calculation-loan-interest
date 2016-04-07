e0e7161e7263b6fb9cb254c53023a922449069ab
Sudan Vellakovil Kanaka Vel <sudan.kanakavel@maplebots.com>
2016-04-07 14:07:50 +0530

    Added files via upload

--- app/index.js
+++ app/index.js
@@ -19,23 +19,81 @@ alert("The form was submitted");
 
     	return (
             <div className= "jumbotron col-sm-6 col-sm-offset-3 text-center"> 
-             <form onSubmit={this.calInterest}>
-            <h2>An React  App For Calculating Your Monthly Loan Interest </h2>
-            <br />
-            <br />
-            <br />
+             <form name="myForm" onSubmit= {this.calInterest}>
+            <h3>An React  App For Calculating Your Monthly Loan Interest </h3>
            
-            <label>Enter loan amount :</label> &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;
-            <input type="text" class="form-control" />
-            <br />
-            <br />
-            <label>Enter  interest rate: </label>&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <input type="text" class="form-control" />
-             <br />
-             <br />
-            <label>Enter duration in months: </label>&nbsp;
-            <input type="text" class="form-control" />
+            
+           <div class="form-group">
+            <label>Choose a bank  :</label>
+              <div class="col-sm-6">
+            <select>
+  <option value="">Canadian Imperial Bank of Commerce</option>
+  <option value="">Bank of Montreal</option>
+  <option value="">Laurentian Bank of Canada</option>
+  <option value="">National Bank of Canada</option>
+</select>
+</div>
+</div>
+           <br />
+           <div class="form-group">
+            <label>Choose a Gender :</label>
+              <div class="col-sm-6">
+              <select>
+  <option value="">Male</option>
+  <option value="">Female</option>
+  <option value="">Other</option>
 
+</select>
+</div>
+</div>
+<br />
+             <div class="form-group">
+            <label>Choose a loan type  :</label>
+             <div class="col-sm-6">
+            <select>
+  <option value="">Personal</option>
+  <option value="">Housiing</option>
+  <option value="">Vechile</option>
+  <option value="">Business</option>
+</select>
+</div>
+</div>
+           <br />
+
+               <div class="form-group">
+            <label>Enter the current age :</label> 
+            <div class="col-sm-6">  
+            <input type="number" class="form-control" placeholder="eg. 25" />
+         </div>
+         </div>
+           <br />
+           <div class="form-group">
+            <label>Enter monthly income in $:</label> 
+            <div class="col-sm-6"> 
+            <input type="number" class="form-control" placeholder="eg. 30000" />
+            </div>
+            </div>
+              <br />
+         <div class="form-group">
+            <label>Enter loan amount in $:</label> 
+            <div class="col-sm-6"> 
+            <input type="number" class="form-control" placeholder="eg. 1000000" />
+            </div>
+            </div>
+              <br />
+             <div class="form-group">
+            <label>Enter  interest rate in % : </label>
+             <div class="col-sm-6"> 
+            <input type="text" class="form-control" placeholder="eg. 10.5" />
+            </div>
+            </div>
+             <br />
+           <div class="form-group">
+            <label>Enter duration in months : </label>
+            <div class="col-sm-6"> 
+            <input type="number" class="form-control" placeholder="eg. 48" />
+           </div>
+            </div>
             <br />
               <br />
               <br />
