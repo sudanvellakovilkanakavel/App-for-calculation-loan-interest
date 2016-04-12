424b1a068577b8e642258a7f9d363f8a7d589b70
Sudan Vellakovil Kanaka Vel <sudan.kanakavel@maplebots.com>
2016-04-12 15:19:05 +0530

    Added files via upload

---
+++ app/components/Result.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var Result = React.createClass({
+
+render: function ()  {
+
+	return (
+
+         <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+
+              console.log('hi sudan');
+              
+         </div>
+
+
+		)
+
+
+
+}
+
+
+
+});
+
+module.exports = Result;
